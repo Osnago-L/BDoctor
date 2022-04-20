8f5cfb6e6bf33d41a204be597f202d06d05ffb3c
Osnago-L <osna.brera@gmail.com>
2022-04-20 18:47:31 +0200

    added bootstrap-vue

--- resources/js/guest/guest.js
+++ resources/js/guest/guest.js
@@ -1,16 +1,26 @@
+// VUE 
 window.Vue = require('vue');
-
-//Axios per front
-window.axios = require('axios');
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import App from './App.vue';
 import router from './router';
-
 
 const app = new Vue({
     el: '#app',
     render: h => h(App),
     router
 });
+
+
+// BOOTSTRAP-VUE 
+// require('./bootstrap');
+// import "./bootstrap"
+import BootstrapVue from 'bootstrap-vue'
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+Vue.use(BootstrapVue)
+
+//AXIOS FRONT
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
