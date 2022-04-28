7ed0daa6c523e26d5d09a70d2cb648ff14a10115
marioBMB <m.sannicola@studenti.uniba.it>
2022-04-28 10:43:55 +0200

    added footer

--- resources/js/guest/App.vue
+++ resources/js/guest/App.vue
@@ -1,17 +1,20 @@
 <template>
     <div>
         <Main/>
+        <Footer />
     </div>
 </template>
 
 <script>
 
 import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
     name: 'App',
     components:{
-        Main
+        Main,
+        Footer,
     }
 }
 </script>
