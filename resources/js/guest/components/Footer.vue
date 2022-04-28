e56637529956df1410c1f5ec831414a1e40cb23d
marioBMB <m.sannicola@studenti.uniba.it>
2022-04-28 15:53:44 +0200

    added footer

--- resources/js/guest/components/Footer.vue
+++ resources/js/guest/components/Footer.vue
@@ -1,28 +1,40 @@
 <template>
   
-    <footer class='m-0-auto'>
+    <footer>
 
-        <div class="top">
+        <div class="top d-flex flex-wrap p-2">
 
-            <div class="top left d-flex col-md-7 col-sm-12">
-
-                <ul>
-                    <li>Email:</li>
-                    <a href="mailto:bdoctors.sfobs@example.it">bdoctors.sfobs@example.it</a>
-
-                </ul>
-
-                <div class="lang">
-                    <img src="img/ita.png" alt="">
-                    <span>Italiano</span>
-                    <i class="fa fa-chevron-down"></i>
-                </div>
+            <div class="top-left d-flex align-items-center justify-content-md-around justify-content-sm-between col-md-6 col-sm-12">
                 
+                <div class='col-auto'>
+
+                    <div class="lang mb-1">
+                        <a class='no-link' href="#">
+                            <span>Italiano</span>
+                            <i class="fa fa-chevron-down"></i>
+                        </a>
+                    </div>
+
+                    <div class='site-logo'>
+                        <a href="#">
+                            <img class="d-none d-sm-inline" src="../../../../public/img/logo_inverted.png" alt="">
+                            <img class="d-inline d-sm-none" src="../../../../public/img/logo_inverted_B.png" alt="">
+                        </a>
+                    </div>
+                </div>
+
+                
+                <div class="contacts col-auto">
+                    <ul>
+                        <li><label><i class="fa fa-envelope" aria-hidden="true"></i></label> <a href="mailto:bdoctor.sfobs@example.it">bdoctor.sfobs@example.it</a></li>
+                        <li><label><i class="fas fa-map-location-dot"></i></label> Vi(c)olino S. Barbi 51, Firenze (FI)</li>
+                    </ul>
+                </div>
             </div>
 
-            <div class="top right d-flex col-md-5 col-sm-12">
+            <div class="top-right d-flex align-items-center justify-content-lg-start my-sm-3 col-md-6 col-sm-12">
 
-                <section class="social-links">
+                <section class="social-links col-3 d-flex justify-content-between">
 
                     <a href="#" class='link-box'>
                         <i class="fab fa-twitter"></i>
@@ -37,12 +49,18 @@
                         <i class="fab fa-youtube"></i>
                     </a>
                 </section>
+
+                <section class='site-logo'>
+
+                </section>
             </div>
         </div>
 
-        <div class="bottom">
-            <p>&copy; 2022 - All right reserved <br/> 
-            <abbr title="Salvalaggio Ficuciello Osnago Barbi Sannicola">SFOBS</abbr>, <abbr title="Boolean Doctors">BDoctor</abbr></p>
+        <div class="bottom d-flex align-items-center justify-content-center">
+            
+            <p>&copy; 2022 - Tutti i diritti riservati –
+                <abbr title="Boolean Doctors">BDoctor</abbr>, <abbr title="Salvalaggio Ficuciello Osnago Barbi Sannicola">SFOBS#51</abbr>
+            </p>
         </div>
 
     </footer>
@@ -61,18 +79,82 @@ export default {
 
     footer {
         width: 100%;
-        
+    
+        .top {
+            background-color: $ms_blue;
+            min-height: 150px;
+            margin: 0 auto;
+
+            .contacts {
+                color: #bbb;
+
+                ul {
+                    margin: 0;
+                    padding: 0;
+
+                    li {
+                        margin: 3px 0;
+                    }
+                }
+            }
+
+            .lang {
+                color: #222;
+                padding: 0;
+            }
+        }
+
+        .bottom {
+            padding: 0.5rem;
+            text-align: center;
+            width: 100%;
+            background-color: $ms_yellow_dark;
+
+            p {
+                margin: 0;
+                padding: 0;
+            }   
+        }
+
+        ul {
+            list-style-type: none;
+        }
+
+        abbr {
+            font-weight: bold;
+            color: $ms_blue;
+        }
+
+        label {
+            font-weight: bolder;
+            padding: 0;
+            margin-right: 5px;
+        }
+
+        .social-links i:hover, a:hover {
+            color: $ms_yellow;
+            transform: translate(-1px, -1px);
+        }
+
+        .social-links {
+            i {
+                font-size: 1.5rem;
+                margin: 0 3px;
+            }
+        }
+
+        a.no-link {
+            color: inherit;
+            text-decoration: none;
+        }
+
+        a {
+            text-decoration: none;
+        }
     }
 
-    footer.bottom {
-        height: 100px;
-        text-align: center;
-    }
+    
 
-    footer.top {
-        background-color: #184370;
-        height: 200px;
-        width: 100%;
-    }
+
 
 </style>
